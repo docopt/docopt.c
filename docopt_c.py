b2a487f0039a235b7314970e296fcbe4f50ae771
Vladimir Keleshev <halst@ya.ru>
2012-06-24 20:57:00 +0200

    First code-generator; handles options only.

--- docopt_c.py
+++ docopt_c.py
@@ -1,4 +1,4 @@
-#include <stdio.h>
+r"""#include <stdio.h>
 #include <stdlib.h>
 #include <stdbool.h>
 #include <string.h>
@@ -175,57 +175,27 @@ TokenStream parse_args(TokenStream ts, Element options[]) {
 /* This is how the generated struct may look like */
 typedef struct {
     /* flag options */
-    int help;
-    int version;
-    int tcp;
-    int serial;
+    <<<flag_options>>>;
     /* options with arguments */
-    char *host;
-    char *port;
-    char *timeout;
-    char *baud;
+    <<<options_with_arguments>>>;
     /* special */
     char *usage_pattern;
     char *help_message;
 } DocoptArgs;
 
 char *help_message =
-"Usage:\n"
-"    the_program --tcp [--host=HOST] [--port=PORT] [--timeout=SECONDS]\n"
-"    the_program --serial [--port=PORT] [--baud=BAUD] [--timeout=SECONDS]\n"
-"    the_program -h | --help | --version\n"
-"\n"
-"Options:\n"
-"  -h, --help             Show this screen.\n"
-"  --version              Print version and exit.\n"
-"  --tcp                  TCP mode.\n"
-"  --serial               Serial mode.\n"
-"  --host HOST            Target host [default: localhost].\n"
-"  -p, --port PORT        Target port [default: 1234].\n"
-"  -t, --timeout SECONDS  Timeout time in seconds [default: 10]\n"
-"  -b, --baud BAUD        Target port [default: 9600].\n"
-"";
+<<<help_message>>>;
 
 char *usage_pattern =
-"Usage:\n"
-"    the_program --tcp [--host=HOST] [--port=PORT] [--timeout=SECONDS]\n"
-"    the_program --serial [--port=PORT] [--baud=BAUD] [--timeout=SECONDS]\n"
-"    the_program -h | --help | --version";
+<<<usage_pattern>>>;
 
 DocoptArgs docopt(int argc, char *argv[], bool help, char *version) {
     DocoptArgs args = {
-        0, 0, 0, 0, "localhost", "1234", "10", "9600",
+        <<<defaults>>>,
         usage_pattern, help_message
     };
     Element options[] = {
-        {Option, {"-h", "--help", 0, 0, NULL}},
-        {Option, {NULL, "--version", 0, 0, NULL}},
-        {Option, {NULL, "--tcp", 0, 0, NULL}},
-        {Option, {NULL, "--serial", 0, 0, NULL}},
-        {Option, {NULL, "--host", 1, 0, NULL}},
-        {Option, {"-p", "--port", 1, 0, NULL}},
-        {Option, {"-t", "--timeout", 1, 0, NULL}},
-        {Option, {"-b", "--baud", 1, 0, NULL}},
+        <<<options>>>,
         {None}
     };
     TokenStream ts = TokenStream_create(argc, argv);
@@ -241,24 +211,83 @@ DocoptArgs docopt(int argc, char *argv[], bool help, char *version) {
                            && strcmp(o->option.olong, "--version") == 0) {
             printf("%s", version);
             exit(0);
-        } else if (strcmp(o->option.olong, "--help") == 0) {
-            args.help = o->option.value;
-        } else if (strcmp(o->option.olong, "--version") == 0) {
-            args.version = o->option.value;
-        } else if (strcmp(o->option.olong, "--tcp") == 0) {
-            args.tcp = o->option.value;
-        } else if (strcmp(o->option.olong, "--serial") == 0) {
-            args.serial = o->option.value;
-        }  else if (o->option.argument && strcmp(o->option.olong, "--host") == 0) {
-            args.host = o->option.argument;
-        } else if (o->option.argument && strcmp(o->option.olong, "--port") == 0) {
-            args.port = o->option.argument;
-        } else if (o->option.argument && strcmp(o->option.olong, "--timeout") == 0) {
-            args.timeout = o->option.argument;
-        } else if (o->option.argument && strcmp(o->option.olong, "--baud") == 0) {
-            args.baud = o->option.argument;
-        }
+        }<<<if_flag>>> <<<if_not_flag>>>
         o = &options[++i];
     }
     return args;
 }
+
+"""
+import sys
+from docopt import (
+        printable_usage, parse_doc_options, parse_pattern, formal_usage,
+)
+
+
+def to_c(s):
+    if type(s) is str:
+        return ('"%s"' % s.replace('\\', r'\\')\
+                          .replace('"', r'\"')\
+                          .replace('\n', '\\n"\n"'))
+    if type(s) in [int, long, float]:
+        return str(s)
+    if s is True:
+        return '1'
+    if s is False:
+        return '0'
+    if s is None:
+        return 'NULL'
+    raise ValueError("can't convert to c type: %r" % s)
+
+
+def c_option(o):
+    return '{Option, {%s}}' % ', '.join(to_c(v) for v in
+             (o.short, o.long, o.argcount, False, None))
+
+
+def c_name(s):
+    return ''.join(c if c.isalnum() else '_' for c in s).strip('_')
+
+
+def c_if_flag(o):
+    t = """ else if (strcmp(o->option.o%s, %s) == 0) {
+            args.%s = o->option.value;\n        }"""
+    return t % (('long' if o.long else 'short'),
+                to_c(o.long or o.short),
+                c_name(o.long or o.short))
+
+
+def c_if_not_flag(o):
+    t = """ else if (o->option.argument && strcmp(o->option.o%s, %s) == 0) {
+            args.%s = o->option.argument;\n        }"""
+    return t % (('long' if o.long else 'short'),
+                to_c(o.long or o.short),
+                c_name(o.long or o.short))
+
+
+if __name__ == '__main__':
+    help_message=sys.stdin.read()
+    usage_pattern = printable_usage(help_message)
+    options = parse_doc_options(help_message)
+    formal_pattern = parse_pattern(formal_usage(usage_pattern), options=options)
+    formal_pattern.fix()
+
+    out = __doc__
+    out = out.replace('<<<flag_options>>>',
+                      ';\n    '.join('int %s' % c_name(o.long or o.short)
+                                     for o in options if o.argcount == 0))
+    out = out.replace('<<<options_with_arguments>>>',
+                      ';\n    '.join('char *%s' % c_name(o.long or o.short)
+                                     for o in options if o.argcount == 1))
+    out = out.replace('<<<help_message>>>', to_c(help_message))
+    out = out.replace('<<<usage_pattern>>>', to_c(usage_pattern))
+    out = out.replace('<<<defaults>>>',
+                      ', '.join(to_c(o.value) for o in
+                                sorted(options, key=lambda o: o.argcount)))
+    out = out.replace('<<<options>>>',
+                      ',\n        '.join(c_option(o) for o in options))
+    out = out.replace('<<<if_flag>>>',
+            ''.join(c_if_flag(o) for o in options if o.argcount == 0))
+    out = out.replace('<<<if_not_flag>>>',
+            ''.join(c_if_not_flag(o) for o in options if o.argcount == 1))
+    print(out.strip())
