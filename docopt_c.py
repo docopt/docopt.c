6d760b7d776e47d1e9e696ed4fa28c08e915d04f
Filipe Funenga <fmafunenga@gmail.com>
2013-06-08 22:03:57 +0100

    Fixes template's tags alignment
    Tags were not in the right place.

--- docopt_c.py
+++ docopt_c.py
@@ -25,237 +25,8 @@ Arguments:
 """
 
 
-TEMPLATE_C = r"""#ifdef __cplusplus
-#include <cstdio>
-#include <cstdlib>
-#include <cstring>
-#else
-#include <stdio.h>
-#include <stdlib.h>
-#include <stdbool.h>
-#include <string.h>
-#endif
-
-
-typedef enum {Option, Argument, Command, None} ElementType;
-
-typedef struct {
-    ElementType type;
-    /*
-     * Should probably be union for storage efficiency, but during development
-     * it's easier to work without it.
-     */
-    /* union { */
-        struct {
-            const char *oshort;
-            const char *olong;
-            bool argcount;
-            bool value;
-            char *argument;
-        } option;
-        struct {
-            char *name;
-            bool repeating;
-            char *value;
-            char **array;
-        } argument;
-        struct {
-            char *name;
-            bool value;
-        } command;
-    /* } data; */
-} Element;
-
-
-/*
- * Tokens object
- */
-
-typedef struct Tokens {
-    int argc;
-    char **argv;
-    int i;
-    char *current;
-} Tokens;
-
-Tokens* tokens_new(Tokens *ts, int argc, char **argv) {
-    struct Tokens update = {argc, argv, 0, argv[0]};
-    (*ts) = update;
-    return ts;
-}
-
-Tokens* tokens_move(Tokens *ts) {
-    if (ts->i < ts->argc) {
-        ts->current = ts->argv[++ts->i];
-    }
-    if (ts->i == ts->argc) {
-        ts->current = NULL;
-    }
-    return ts;
-}
-
-
-/*
- * ARGV parsing functions
- */
-
-Tokens* parse_shorts(Tokens *ts, Element options[]) {
-    Element *o;
-    char *raw;
-
-    raw = &ts->current[1];
-    tokens_move(ts);
-    while (raw[0] != '\0') {
-        int i = 0;
-        o = &options[i];
-        while (o->type != None) {
-            if (o->type == Option && o->option.oshort != NULL
-                                  && o->option.oshort[1] == raw[0]) {
-                break;
-            }
-            o = &options[++i];
-        }
-        if (o->type == None) {  // TODO -%s is specified ambiguously %d times
-            printf("-%c is not recognized", raw[0]);
-            exit(1);
-        }
-        raw++;
-        if (!o->option.argcount) {
-            o->option.value = true;
-        } else {
-            if (raw[0] == '\0') {
-                if (ts->current == NULL) {
-                    printf("%s requires argument", o->option.oshort);
-                    exit(1);
-                }
-                raw = ts->current;
-                tokens_move(ts);
-            }
-            o->option.argument = raw;
-            break;
-        }
-    }
-    return ts;
-}
-
-Tokens* parse_long(Tokens *ts, Element options[]) {
-    char *eq = strchr(ts->current, '=');
-    char *argument = NULL;
-    int i = 0;
-    Element *o;
-
-    if (eq != NULL) {
-        *eq = '\0'; // "--option=value\0" => "--option\0value\0"
-        argument = eq + 1;
-    }
-    o = &options[i];
-    while (o->type != None) {
-        if (o->type == Option &&
-                strncmp(ts->current, o->option.olong, strlen(ts->current)) == 0) {
-            break;
-        }
-        o = &options[++i];
-    }
-    if (o->type == None) {  // TODO '%s is not a unique prefix
-        printf("%s is not recognized", ts->current);
-        exit(1);
-    }
-    tokens_move(ts);
-    if (o->option.argcount) {
-        if (argument == NULL) {
-            if (ts->current == NULL) {
-                printf("%s requires argument", o->option.olong);
-                exit(1);
-            }
-            o->option.argument = ts->current;
-            tokens_move(ts);
-        } else {
-            o->option.argument = argument;
-        }
-    } else {
-        if (argument != NULL) {
-            printf("%s must not have an argument", o->option.olong);
-            exit(1);
-        }
-        o->option.value = true;
-    }
-    return ts;
-}
-
-Tokens* parse_args(Tokens *ts, Element options[]) {
-    while (ts->current != NULL) {
-        if (strcmp(ts->current, "--") == 0) {
-            // not implemented yet
-            return ts;
-            //return parsed + [Argument(None, v) for v in tokens]
-        } else if (ts->current[0] == '-' && ts->current[1] == '-') {
-            parse_long(ts, options);
-        } else if (ts->current[0] == '-' ) {
-            parse_shorts(ts, options);
-        } else {
-            // not implemented yet
-            tokens_move(ts); // just skip for now
-            //parsed.append(Argument(None, tokens.move()))
-        }
-    }
-    return ts;
-}
-
-
-/*
- * Main docopt function
- */
-
-typedef struct {
-    /* flag options */
-    $flag_options;
-    /* options with arguments */
-    $options_with_arguments;
-    /* special */
-    const char *usage_pattern;
-    const char *help_message;
-} DocoptArgs;
-
-const char help_message[] =
-$help_message;
-
-const char usage_pattern[] =
-$usage_pattern;
-
-DocoptArgs docopt(int argc, char *argv[], bool help, const char *version) {
-    int i = 0;
-    Tokens ts;
-    DocoptArgs args = {
-        $defaults,
-        usage_pattern, help_message
-    };
-    Element options[] = {
-        $options,
-        {None}
-    };
-    Element *o;
-
-    tokens_new(&ts, argc, argv);
-    parse_args(&ts, options);
-    o = &options[i];
-    while (o->type != None) {
-        if (help && o->option.value
-                 && strcmp(o->option.olong, "--help") == 0) {
-            printf("%s", args.help_message);
-            exit(0);
-        } else if (version && o->option.value
-                           && strcmp(o->option.olong, "--version") == 0) {
-            printf("%s", version);
-            exit(0);
-        }$if_flag $if_not_flag
-        o = &options[++i];
-    }
-    return args;
-}
-"""
-
-
 import sys
+import os.path
 import re
 import docopt
 from string import Template
@@ -314,8 +85,10 @@ if __name__ == '__main__':
             sys.exit("")
         else:
             args['<docopt>'] = sys.stdin.read()
-        if args['--template'] is not None:
-            with open(args['--template'], 'r') as f:
+        if args['--template'] is None:
+            args['--template'] = os.path.join(
+                    os.path.dirname(os.path.realpath(__file__)), "template.c")
+        with open(args['--template'], 'r') as f:
                 args['--template'] = f.read()
     except IOError as e:
         sys.exit(e)
@@ -331,24 +104,33 @@ if __name__ == '__main__':
 
     options = docopt.parse_defaults(doc)
     pattern = docopt.parse_pattern(docopt.formal_usage(usage), options)
-    defaults = ', '.join(to_c(o.value) for o in sorted(options, key=lambda o: o.argcount))
-    defaults = re.sub(r'"(.*?)"', r'(char*) "\1"', defaults)
 
-    out = TEMPLATE_C if args['--template'] is None else args['--template']
-    out = Template(out).safe_substitute(
-            flag_options=';\n    '.join(
-                    'int %s' % c_name(o.long or o.short)
-                    for o in options if o.argcount == 0),
-            options_with_arguments=';\n    '.join(
-                    'char *%s' % c_name(o.long or o.short)
-                    for o in options if o.argcount == 1),
+    flag_options = ';\n    '.join('int %s' % c_name(o.long or o.short)
+                                  for o in options if o.argcount == 0)
+    flag_options = (('\n    /* flag options */\n    ' + flag_options + ';')
+                    if flag_options != '' else '')
+    opts_with_arguments = ';\n    '.join('char *%s' % c_name(o.long or o.short)
+                                         for o in options if o.argcount == 1)
+    opts_with_arguments = (('\n    /* options with arguments */\n    ' +
+                            opts_with_arguments + ';')
+                           if opts_with_arguments != '' else '')
+    defaults = ', '.join(to_c(o.value)
+                         for o in sorted(options, key=lambda o: o.argcount))
+    defaults = re.sub(r'"(.*?)"', r'(char*) "\1"', defaults)
+    defaults = ('\n        ' + defaults + ',') if defaults != '' else ''
+    opts = ',\n        '.join(c_option(o) for o in options)
+    opts = ('\n        ' + opts + ',') if opts != '' else ''
+
+    out = Template(args['--template']).safe_substitute(
+            flag_options=flag_options,
+            options_with_arguments=opts_with_arguments,
             help_message=to_c(doc),
             usage_pattern=to_c(usage),
             defaults=defaults,
-            options=',\n        '.join(c_option(o) for o in options),
+            options=opts,
             if_flag=''.join(c_if_flag(o) for o in options if o.argcount == 0),
-            if_not_flag=''.join(
-                    c_if_not_flag(o) for o in options if o.argcount == 1))
+            if_not_flag=''.join(c_if_not_flag(o)
+                                for o in options if o.argcount == 1))
 
     if args['--output-name'] is None:
         print(out.strip() + '\n')
