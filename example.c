09e779296f67754c4f8312512fdcb9136a8a75c0
Filipe Funenga <fmafunenga@gmail.com>
2013-06-09 22:41:26 +0100

    Changes example to the Naval Fate example
    Because it includes all the types of leaf patterns.

--- example.c
+++ example.c
@@ -4,13 +4,23 @@ int main(int argc, char *argv[])
 {
     DocoptArgs args = docopt(argc, argv, /* help */ 1, /* version */ "2.0rc2");
 
-    printf("--help == %s\n", args.help ? "true" : "false");
-    printf("--version == %s\n", args.version ? "true" : "false");
-    printf("--tcp == %s\n", args.tcp ? "true" : "false");
-    printf("--serial == %s\n", args.serial ? "true" : "false");
-    printf("--host == %s\n", args.host);
-    printf("--port == %s\n", args.port);
-    printf("--timeout == %s\n", args.timeout);
-    printf("--baud == %s\n", args.baud);
+    printf("Commands\n");
+    printf("    mine == %s\n", args.mine ? "true" : "false");
+    printf("    move == %s\n", args.move ? "true" : "false");
+    printf("    create == %s\n", args.create ? "true" : "false");
+    printf("    remove == %s\n", args.remove ? "true" : "false");
+    printf("    set == %s\n", args.set ? "true" : "false");
+    printf("    ship == %s\n", args.ship ? "true" : "false");
+    printf("    shoot == %s\n", args.shoot ? "true" : "false");
+    printf("Arguments\n");
+    printf("    x == %s\n", args.x);
+    printf("    y == %s\n", args.y);
+    printf("Flags\n");
+    printf("    --drifting == %s\n", args.drifting ? "true" : "false");
+    printf("    --help == %s\n", args.help ? "true" : "false");
+    printf("    --moored == %s\n", args.moored ? "true" : "false");
+    printf("    --version == %s\n", args.version ? "true" : "false");
+    printf("Options\n");
+    printf("    --speed == %s\n", args.speed);
     return 0;
 }
