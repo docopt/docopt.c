4a766524504805e09aaaa4872d46fe5c91cd6fa5
Samuel Marks <807580+SamuelMarks@users.noreply.github.com>
2020-05-02 21:17:34 +1000

    [template.h,template.c] On second thought, `struct DocoptArgs` and `struct DocoptArgs docopt` should be the only public

--- template.c
+++ template.c
@@ -6,6 +6,46 @@
 
 #include "$header_name"
 
+struct Command {
+    const char *name;
+    bool value;
+};
+
+struct Argument {
+    const char *name;
+    char *value;
+    char **array;
+};
+
+struct Option {
+    const char *oshort;
+    const char *olong;
+    bool argcount;
+    bool value;
+    char *argument;
+};
+
+struct Elements {
+    size_t n_commands;
+    size_t n_arguments;
+    size_t n_options;
+    struct Command *commands;
+    struct Argument *arguments;
+    struct Option *options;
+};
+
+
+/*
+ * Tokens object
+ */
+
+struct Tokens {
+    size_t argc;
+    char **argv;
+    size_t i;
+    char *current;
+};
+
 const char help_message[] =
 $help_message;
 
--- template.h
+++ template.h
@@ -9,46 +9,6 @@ struct DocoptArgs {$commands$arguments$flags$options
     const char *help_message;
 };
 
-struct Command {
-    const char *name;
-    bool value;
-};
-
-struct Argument {
-    const char *name;
-    char *value;
-    char **array;
-};
-
-struct Option {
-    const char *oshort;
-    const char *olong;
-    bool argcount;
-    bool value;
-    char *argument;
-};
-
-struct Elements {
-    size_t n_commands;
-    size_t n_arguments;
-    size_t n_options;
-    struct Command *commands;
-    struct Argument *arguments;
-    struct Option *options;
-};
-
-
-/*
- * Tokens object
- */
-
-struct Tokens {
-    size_t argc;
-    char **argv;
-    size_t i;
-    char *current;
-};
-
 struct DocoptArgs docopt(size_t,char *[],bool,const char *);
 
 #endif
